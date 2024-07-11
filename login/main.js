4650632d6c54f0a5fa4dc40a5b3e3b2a00cfe2df
Chaymabaroumi <chaymabaroumi984@gmail.com>
2024-07-11 18:21:52 +0100

    login done

---
+++ login/main.js
@@ -0,0 +1,13 @@
+document.getElementById('signin-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    alert('Sign in succeeded!');
+});
+document.querySelector('.social-button.facebook').addEventListener('click', function() {
+    window.location.href = 'https://www.facebook.com';
+});
+
+document.querySelector('.social-button.instagram').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/traveltodo/';
+});
